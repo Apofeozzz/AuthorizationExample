26a5543be79f13b0a400ab6b6bfeb3d9e27cac64
Denis Grishchenko <denis.grishchenko.ios@gmail.com>
2020-05-19 15:16:14 +0300

    Commit before merge

---
+++ Pods/Target Support Files/Pods-AuthorizationExample/Pods-AuthorizationExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AuthorizationExample : NSObject
+@end
+@implementation PodsDummy_Pods_AuthorizationExample
+@end
---
+++ Pods/Target Support Files/Pods-AuthorizationExampleTests/Pods-AuthorizationExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AuthorizationExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_AuthorizationExampleTests
+@end
